6783ce68931656ed0f1bdc98fdae3f16ebc67e81
ebstiv23 <ebs9787@g.rit.edu>
2016-03-06 16:06:41 -0500

    HUD responds to input, 4 inputs displayed, dragon waypoint model added.

--- DragonTrainer/Assets/Custom Scripts/CommandControls.cs
+++ DragonTrainer/Assets/Custom Scripts/CommandControls.cs
@@ -6,40 +6,49 @@ public class CommandControls : MonoBehaviour {
 
     public Dragon dragon; // set in inspector
     public Canvas canvas; //set in the inspector
-    public GameObject[] command = new GameObject[3];
-    public Texture[] comText = new Texture[6];
+    public GameObject[] command = new GameObject[3]; //the HUD elements that we will update
+    public Texture[] comText = new Texture[6]; // the textures we will use on the HUD
+
+    KeyCode prevInput; // make sure we don't spamm the program with repeat info
 	// Use this for initialization
 	void Start () {
+        //setting the textures to a default value
         command[0].transform.GetComponent<UnityEngine.UI.RawImage>().texture = comText[0];
-        command[1].transform.GetComponent<UnityEngine.UI.RawImage>().texture = comText[1];
-        command[2].transform.GetComponent<UnityEngine.UI.RawImage>().texture = comText[5];
+        command[1].transform.GetComponent<UnityEngine.UI.RawImage>().texture = comText[3];
+        command[2].transform.GetComponent<UnityEngine.UI.RawImage>().texture = comText[4];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            dragon.Command(1);
-            print("one pressed");
+            dragon.Command(1); //tell the dragon our command
+            UpdateHUD(1); //update the hud
+            prevInput = KeyCode.Alpha1; //record our keypress
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2) && prevInput != KeyCode.Alpha2)
         {
-            print("two pressed");
+            UpdateHUD(2); //update the hud
+            prevInput = KeyCode.Alpha2; //record our keypress
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            print("three pressed");
+            UpdateHUD(3); //update the hud
+            prevInput = KeyCode.Alpha3; //record our keypress
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            print("four pressed");
+            UpdateHUD(4); //update the hud
+            prevInput = KeyCode.Alpha4; //record our keypress
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            print("five pressed");
+            UpdateHUD(5); //update the hud
+            prevInput = KeyCode.Alpha5; //record our keypress
         }
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
-            print("six pressed");
+            UpdateHUD(6); //update the hud
+            prevInput = KeyCode.Alpha6; //record our keypress
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
@@ -50,4 +59,17 @@ public class CommandControls : MonoBehaviour {
             print("Disapproval Key Pressed");
         }
 	}
+
+    //for visual feedback on the HUD
+    void UpdateHUD(int input){
+        Texture next; //stores what the next image should be
+        Texture current = comText[input - 1]; //command 1 is at index 0
+        for(int i = 0; i < command.Length; i++)
+        {
+            next = command[i].transform.GetComponent<UnityEngine.UI.RawImage>().texture;
+            command[i].transform.GetComponent<UnityEngine.UI.RawImage>().texture = current;
+            current = next;
+        }
+
+    }
 }
