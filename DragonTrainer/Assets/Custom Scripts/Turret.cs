6f0c2dc99fe014c834e6ec26e808ce22bed0efba
Ben <bxc3201@rit.edu>
2016-04-17 18:06:21 -0400

    turret improved

--- DragonTrainer/Assets/Custom Scripts/Turret.cs
+++ DragonTrainer/Assets/Custom Scripts/Turret.cs
@@ -17,10 +17,40 @@ public class Turret : MonoBehaviour {
 		killTimer += Time.deltaTime;
 		if (killTimer > KillInterval) {
 			killTimer = killTimer - KillInterval; // reset kill timer
-			Vector3 minDist = new Vector3(100000000000,0,0);
-			/*for (EnemyBehavior enemy in manager.enemies) {
-				if (
-			}*/
+
+			// find the closest enemy
+			Vector3 minDistVec = new Vector3(100000000000,0,0);
+			// store the closest
+			EnemyBehavior closest = null;
+
+			foreach (EnemyBehavior e in manager.enemies) {
+				if ((transform.position - e.transform.position).sqrMagnitude < (transform.position - minDistVec).sqrMagnitude) {
+					closest = e;
+					minDistVec = e.transform.position;
+				}
+			}
+
+			//print("closest monster " + closest.id);
+
+			RaycastHit hit;
+
+			//Debug.DrawLine (transform.position, closest.transform.position,Color.red,0.5f);
+
+			Ray myRay = new Ray (transform.position, new Vector3(0,0,5) + closest.transform.position - transform.position);
+
+			Debug.DrawLine (transform.position, transform.position + myRay.direction*(closest.transform.position - transform.position).magnitude,Color.blue,0.5f);
+
+			Physics.Raycast (myRay, out hit, (closest.transform.position - transform.position).magnitude + 10);
+
+			//print (hit.transform + " hit (A)");
+
+			if (hit.transform && hit.transform.gameObject.GetComponent<EnemyBehavior>() != null) {
+				// kill
+				//hit.transform.gameObject.GetComponent<EnemyBehavior>().hp -= 1000000;
+				print (hit.transform + "" + hit.transform.gameObject.GetComponent<EnemyBehavior>().id + " hit");
+			} else {
+				//print (hit.transform + " hit (B)");
+			}
 		}
 	}
 }
